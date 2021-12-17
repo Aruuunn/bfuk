1cf4f1a0e9b220baf53e5668e7b1ebec12b92b16
Arun Murugan <arunmurugan200@gmail.com>
2021-12-17 14:04:39 +0530

    feat: execute a string of bf, print usage, version, etc

--- bfuk/main.go
+++ bfuk/main.go
@@ -2,23 +2,53 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/arunmurugan78/bfuk"
 )
 
+const (
+	version = "1.0.0"
+)
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage:\n\tbfuk filename.bf")
-		return
+	var reader *bufio.Reader
+
+	flag.Usage = func() {
+		fmt.Println("Usage:\n\tbfuk [OPTIONS] COMMAND\nExample:\n\tbfuk /filepath/filename.bf\nOptions:")
+		flag.PrintDefaults()
 	}
 
-	filepath := os.Args[1]
+	bfString := flag.String("e", "", "Execute given string of brainf**k program")
 
-	f, _ := os.Open(filepath)
+	flag.Parse()
 
-	reader := bufio.NewReader(f)
+	if len(*bfString) != 0 {
+		reader = bufio.NewReader(strings.NewReader(*bfString))
+	} else {
+		filepath := flag.CommandLine.Arg(0)
+
+		if len(filepath) == 0 {
+			// When filepath is not specified
+			fmt.Println("bfuk", version)
+			fmt.Println("Description: a Brainf**k programming language interpreter.")
+			fmt.Println("Author: Arun Murugan")
+			flag.Usage()
+			os.Exit(0)
+		}
+
+		file, err := os.Open(filepath)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		reader = bufio.NewReader(file)
+	}
 
 	bf := bfuk.NewBfuk(reader)
 
@@ -26,6 +56,5 @@ func main() {
 	output := bufio.NewWriter(os.Stdout)
 
 	bf.Run(input, output)
-
 	output.Flush()
 }
